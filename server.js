b9306c56e664cb1800b4f36527e051a272534a8f
wyamet <wyattdomanski.md@gmail.com>
2023-02-09 12:51:00 -0800

    added connect-session-sequelize

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
   secret: "Super secret secret",
   cookie: {
-    maxAge: 60 * 60 * 1000,
+    maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: false,
     sameSite: "strict",
