777270fc7181aee145f2359df7f98bf6a2141dd5
wyamet <wyattdomanski.md@gmail.com>
2023-02-09 21:19:09 -0800

    working on login

--- server.js
+++ server.js
@@ -30,8 +30,14 @@ const sess = {
 //set as middleware
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
