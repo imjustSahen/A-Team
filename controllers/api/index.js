e0e014bee2c295cd9de6145cb5bc305e70bcbac1
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-10 15:28:17 -0800

    added routes to api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const pairingRoutes = require("./pairing-routes");
+const reviewRoutes = require("./review-routes");
+const commentRoutes = require("./comment-routes");
 
-//localhost:3001/api/user
+//localhost:3001/api
 router.use("/user", userRoutes);
+router.use("/pairing", pairingRoutes);
+router.use("/review", reviewRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
