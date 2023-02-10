3f5069c7e77e0f0a1517f651534513ce0546fd3d
wyamet <wyattdomanski.md@gmail.com>
2023-02-09 19:17:12 -0800

    added login and logout js files in public folder

---
+++ public/js/login.js
@@ -0,0 +1,6 @@
+const loginForm = async (event) => {
+  event.preventDefault();
+};
+
+const email = document.querySelector("#email-login").value.trim();
+const password = document.querySelector("#password-login").value.trim();
