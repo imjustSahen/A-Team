1a0dc332230f99a03aeaad3a45512617a15b5f8f
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-09 20:24:49 -0800

    userSeeds created

---
+++ seeds/userSeeds.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: "Michael Scott",
+        email: "greatscott@email.com",
+        password: "No0neWilLGuess",
+    },
+    {
+        username: "Dwight Schrute",
+        email: "bestbeets@email.com",
+        password: "BestBEETS"
+    },
+    {
+        username: "Jimothy Halpert",
+        email: "ilikesports@email.com",
+        password: "SPORTSSS"
+    }
+];
+
+const seedGallery = () => User.bulkCreate(userData);
+
+module.exports = seedGallery;
