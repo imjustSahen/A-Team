2b4def0034c482dcb5daecd9d4fdf94f47bb21e0
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-13 15:31:20 -0800

    added logout route for User API routing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -121,4 +121,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//logout route
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    })
+  } else {
+    res.status(404).end();
+  }
+})
 module.exports = router ;
