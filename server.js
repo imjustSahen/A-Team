b63835d72c6a73fb795e067b1ea398897d0c5a8a
wyamet <wyattdomanski.md@gmail.com>
2023-02-13 17:16:13 -0800

    updated server.js, added partials folder to views

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const path = require("path");
 const express = require("express");
 //import session
 const session = require("express-session");
+//Loads the handlebars module
 const exphbs = require("express-handlebars");
-
+//Require the connect-session-sequelize and use it to create a custom store for Express.js sessions
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require("./controllers/routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 
 const app = express();
+// sets the port to listen too
 const PORT = process.env.PORT || 3001;
+
 //set up sessions
 const sess = {
   secret: "Super secret secret",
@@ -30,17 +33,33 @@ const sess = {
 //set as middleware
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+//sets the hhelpers module to be used in the templates. we currently dont have any helpers so I commented it out for now.
+// const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+//sets app to use handlebars
+app.set("view engine", "hbs");
+//Sets handlebars configurations
+app.engine(
+  "handlebars",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
 
 app.use(express.json());
+//Use the express.urlencoded middleware to parse incoming request bodies in the URL-encoded format
 app.use(express.urlencoded({ extended: true }));
+//Serves static files (so we can access the public folder)
 app.use(express.static(path.join(__dirname, "public")));
-
+//Use the routes module to handle incoming HTTP requests
 app.use(routes);
 
+app.get("/", (req, res) => {
+  //Sends the body of the page  "main.handlebars" to the container "index.handlebars"
+  res.render("main", { layout: "index" });
+});
+
+// syncs sequilize with the DB without overwriting exisiting data, and start the app.
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
