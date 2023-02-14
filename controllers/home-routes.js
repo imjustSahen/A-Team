4a080476a849cc7c367f93913dec7d9aa4abff45
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-13 20:25:04 -0800

    added routing to controllers/index.js . added GET route for all pairings on home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,11 +3,27 @@ const path = require("path");
 const { User, Pairing, Comment, Review } = require('../models');
 const sequelize = require('sequelize');
 
+router.get('/', async (req, res) => {
+  try {
+      const pairingData = await Pairing.findAll({
+          include: [
+            {
+              model: User,
+              attributes: {exclude: ['password']}
+            },
+            {
+              model: Review,
+              attributes: {exclude: ['id']}
+            }
+          ]
+      });
 
-// This is the 'get' route
-router.get("/", async (req, res) => {
-  // Here, index.html is rendered
-  res.sendFile(path.join(__dirname, "../views/index.html"));
+      const pairings = await pairingData.map((pairing) => pairing.get({ plain: true}));
+
+      res.status(200).json(pairings);
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 module.exports = router;
