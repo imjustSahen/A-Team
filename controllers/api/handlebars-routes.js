e1560e2537a326e938a609fd861ec2a69f1208e8
wyamet <wyattdomanski.md@gmail.com>
2023-02-16 12:47:40 -0800

    got base for carrousel handlebar partial set, and fixed link to pairing.handlebars

--- controllers/api/handlebars-routes.js
+++ controllers/api/handlebars-routes.js
@@ -3,19 +3,22 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/views/aboutUs", (req, res) => {
-  res.render("aboutUs", { layout: "main" });
+  res.render("aboutUs");
 });
 
 router.get("/", (req, res) => {
-  res.render("home", { layout: "main" });
+  const imagesArr = [{ number: 2 }, { number: 3 }, { number: 4 }];
+  res.render("home", {
+    images: imagesArr,
+  });
 });
 
 router.get("/views/contactUs", (req, res) => {
-  res.render("contactUs", { layout: "main" });
+  res.render("contactUs");
 });
 
 router.get("/views/pairing", (req, res) => {
-  res.render("pairing", { layout: "main" });
+  res.render("pairing");
 });
 
 module.exports = router;
