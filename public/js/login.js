dad40944b1d7a0f4a1d00e56fb3f246fe7278011
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-19 13:45:19 -0800

    functional signup routing/user creation and data transfer to DB

--- public/js/login.js
+++ public/js/login.js
@@ -27,7 +27,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/user", {
       method: "POST",
       body: JSON.stringify({ username, email, password }),
       headers: { "Content-Type": "application/json" },
@@ -42,7 +42,7 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector("#submit")
+  .querySelector("#signinBtn")
   .addEventListener("click", loginFormHandler);
 
 document
