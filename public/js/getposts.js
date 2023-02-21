2ed56c92768eb568b256e2f976189659d8426ec6
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-20 21:25:30 -0800

    added successful fetch for pairing reviews

--- public/js/getposts.js
+++ public/js/getposts.js
@@ -1,15 +1,25 @@
 
 const getPairings = async () => {
-
     const response = await fetch("/api/pairing");
-
 
     var data = await response.json();
     console.log(data);
 
     if(!response.ok) {
-        throw new Error('HTTP error!')
+        throw new Error('Could not fetch');
+    }
+};
+
+const getPairingReviews = async () => {
+    const response = await fetch("/api/review");
+
+    var data = await response.json();
+    console.log(data);
+
+    if(!response.ok){
+        throw new Error('Could not fetch');
     }
 };
 
 getPairings();
+getPairingReviews();
