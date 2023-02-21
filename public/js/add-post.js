cfa8d7bd8d262b2c77c8ef218675d96ca1f0e0c7
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-02-20 19:42:22 -0800

    updated add-post.js for functional pairing saving

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,10 +1,10 @@
 async function savePairings(event) {
     event.preventDefault();
 
-    const beer_id = document.querySelector('#beer-id').value;
-    const beer_img = document.querySelector('#beer-img-url').value;
-    const dish_id = document.querySelector('#dish-id').value;
-    const dish_img = document.querySelector('#dish-img-url').value;
+    const beer_id = document.querySelector('#beer-id').innerHTML;
+    const beer_img = document.querySelector('#beer-img-url').innerHTML;
+    const dish_id = document.querySelector('#dish-id').innerHTML;
+    const dish_img = document.querySelector('#dish-img-url').innerHTML;
 
     const response = await fetch(`/api/pairing`, {
         method: 'POST',
