40de4a9a02073274548a627bd745a967c1e03f54
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2023-02-21 20:55:53 -0800

    updated add-post.js for beer and dish info

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -3,20 +3,25 @@ const savePairing = document.getElementById("save-pairing-btn");
 async function savePairings(event) {
     event.preventDefault();
 
-    console.log('recieved')
-
-    const beer_id = document.getElementById('#beer-id').innerHTML;
-    const beer_img = document.querySelector('#beer-img-url').innerHTML;
+    const beer_name = document.querySelector('.beer-name').innerHTML;
+    const beer_id = document.querySelector('#beer-id').innerHTML;
+    const beer_img = $(".beer-image img").attr("src");
     const dish_id = document.querySelector('#dish-id').innerHTML;
-    const dish_img = document.querySelector('#dish-img-url').innerHTML;
-    console.log(beer_id, dish_id, dish_img)
+    const dish_name = document.querySelector('.dish-name').innerHTML;
+    const dish_img = $(".dish-image img").attr("src");
+    const dish_link = $("#recipe-link").attr("href");
+    console.log(beer_img, dish_img, dish_link)
+
     const response = await fetch(`/api/pairing`, {
         method: 'POST',
         body: JSON.stringify({
         beer_id,
+        beer_name,
         beer_img,
         dish_id,
+        dish_name,
         dish_img,
+        dish_link,
         }),
         headers: {
         'Content-Type': 'application/json'
