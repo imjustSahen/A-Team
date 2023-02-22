a9b5fab0a0ea6bdedfb0a7bbe6beb882bdfee4e0
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2023-02-21 18:51:52 -0800

    home page route adjusted

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,46 @@ const { User, Pairing, Comment, Review } = require("../models");
 const sequelize = require("sequelize");
 const express = require("express");
 
+// Handlebar routes
+router.get("/views/aboutUs", (req, res) => {
+  res.render("aboutUs", { loggedIn: req.session.loggedIn });
+});
+
+router.get("/", (req, res) => {
+  const imagesArr = [{ number: 2 }, { number: 3 }, { number: 4 }];
+  res.render("home", {
+    images: imagesArr,
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+// router.get("/views/login", (req, res) => {
+//   res.render("login", { loggedIn: req.session.loggedIn });
+// });
+
+router.get("/views/contactUs", (req, res) => {
+  res.render("contactUs", { loggedIn: req.session.loggedIn });
+});
+
+router.get("/views/pairing", (req, res) => {
+  res.render("pairing", { loggedIn: req.session.loggedIn });
+});
+
+//once a user is logged in, redirect to home page
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+});
+
+//rendering sign up page
+//once a user clicks sign up or generate pairing will redirect them to the pairings home page
+//with a modal
+router.get("/signup", (req, res) => {
+  // res.redirect('signup')
+});
+
 //getting all pairing data for carousel cards
 router.get("/", async (req, res) => {
   try {
@@ -25,47 +65,6 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-});
-
-//once a user is logged in, redirect to home page
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-});
-
-//rendering sign up page
-//once a user clicks sign up or generate pairing will redirect them to the pairings home page
-//with a modal
-router.get("/signup", (req, res) => {
-  // res.redirect('signup')
-});
-
-// Handlebar routes
-
-router.get("/views/aboutUs", (req, res) => {
-  res.render("aboutUs", { loggedIn: req.session.loggedIn });
-});
-
-router.get("/", (req, res) => {
-  const imagesArr = [{ number: 2 }, { number: 3 }, { number: 4 }];
-  res.render("home", {
-    images: imagesArr,
-    loggedIn: req.session.loggedIn,
-  });
-});
-
-router.get("/views/login", (req, res) => {
-  res.render("login", { loggedIn: req.session.loggedIn });
-});
-
-router.get("/views/contactUs", (req, res) => {
-  res.render("contactUs", { loggedIn: req.session.loggedIn });
-});
-
-router.get("/views/pairing", (req, res) => {
-  res.render("pairing", { loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;
